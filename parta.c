d490a51316003254af280e0ce5671660d80b001e
ismailatibatwit <144959357+ismailatibatwit@users.noreply.github.com>
2025-11-10 10:38:20 -0500

    Refactor main to process and print arguments in uppercase
    Updated main function to handle command line arguments and print them in uppercase, separated by commas.

--- parta.c
+++ parta.c
@@ -1,8 +1,21 @@
-#include <stdbool.h>
-#include <stddef.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 // TODO: Include more header files (read the manpages to find out more!)
 
-int main(int argc, const char* argv[]) {
+
+int main(int argc, const char *argv[]) {
+    if (argc < 2) {
+        printf("ERROR: No arguments\n");
+        return 1;
+    }
+    for (int i = 1; i < argc; i++) {
+        for (int j = 0; argv[i][j] != '\0'; j++) {
+            putchar(toupper((unsigned char)argv[i][j]));
+        }
+        if (i < argc - 1) putchar(',');
+    }
+    putchar('\n');
     return 0;
 }
