b56279670380e1c69e263cc5b7621ef8edea0a0e
ismailatibatwit <144959357+ismailatibatwit@users.noreply.github.com>
2025-11-10 10:39:25 -0500

    Update main function to process command line arguments
    Refactor main function to handle command line arguments and print specific arguments based on their count.

--- partb.c
+++ partb.c
@@ -1,8 +1,25 @@
-#include <stdbool.h>
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // TODO: Include more header files (read the manpages to find out more!)
+int main(int argc, const char *argv[]) {
+    if (argc < 2) {
+        printf("ERROR: No arguments\n");
+        return 1;
+    }
 
-int main(int argc, const char* argv[]) {
+    int n = argc - 1;
+    int mid = n / 2;
+
+    // echo behavior
+    if (n == 1) {
+        printf("%s\n", argv[1]);
+    } else if (n % 2 == 1) {  
+        printf("%s\n", argv[mid + 1]);
+    } else {  
+        printf("%s %s\n", argv[mid], argv[mid + 1]);
+    }
+
     return 0;
 }
